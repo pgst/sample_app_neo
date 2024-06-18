831f76050a35566c62c306670d7aaf83071a60e5
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-06-18 06:02:10 +0000

    7.4 ユーザー登録成功

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # flash[:success] = "Welcome to the Sample App!"
-      # redirect_to @user
+      flash[:success] = "サンプルアプリへようこそ！"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
