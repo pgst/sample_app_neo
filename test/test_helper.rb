63cb3b6b85d3bc4ddf8a4db7ab443a4b3fad7ee6
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-06-20 05:56:03 +0000

    8.2 ログイン

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,18 +3,20 @@ require_relative "../config/environment"
 require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
+include ApplicationHelper
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
+    # 指定されたワーカーでテストを並列実行する
     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    # すべてのテストに対して、test/fixtures/*.yml内の
+    # すべてのフィクスチャをアルファベット順にセットアップ
     fixtures :all
 
-    # Add more helper methods to be used by all tests here...
-
-    # 今回追加
-    include ApplicationHelper
+    # テストユーザーがログインしていればtrueを返す
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
   end
 end
